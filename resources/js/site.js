a0a6c8fc1f84835cfcc90ac5f71a9516e907247f
Jack Whiting <hi@jackwhiting.co.uk>
2022-12-12 15:36:55 +0000

    updating vendor import directorys

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,8 +1,8 @@
-import checkout from './shopify/checkout'
-import cart, { setCartCount } from './shopify/cart'
-import productForm from './shopify/products'
+import checkout from "./vendor/shopify/checkout";
+import cart, { setCartCount } from "./vendor/shopify/cart";
+import productForm from "./vendor/shopify/products";
 
-new checkout()
-new setCartCount()
-new productForm()
-new cart()
+new checkout();
+new setCartCount();
+new productForm();
+new cart();
